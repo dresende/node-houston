42c4f57789c3e99f3bcec7148812a92d98487b89
Diogo Resende <dresende@thinkdigital.pt>
2012-01-23 22:54:08 +0000

    Change log errors event to error

--- examples/events.js
+++ examples/events.js
@@ -3,7 +3,7 @@ var log = require("../lib/houston")("simple");
 log.on("log", function (type, text) {
 	console.log("log - '%s' message: %s", type, text);
 });
-log.on("errorlog", function (type, text) {
+log.on("error", function (type, text) {
 	console.log("error log - message: %s", type, text);
 });
 log.warn("We have a problem!");
