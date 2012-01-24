8a3cf9fa9a5567fdd758fc72db27635c532275bb
Diogo Resende <dresende@thinkdigital.pt>
2012-01-24 22:32:16 +0000

    Add base Transport object for transports to inherit from (to allow common methods) Add Transport.filter() to allow a specific transport to only log certain log uids

---
+++ examples/filter.js
@@ -0,0 +1,17 @@
+var houston = require("../lib/houston"),
+    simplelog = houston("simple"),
+    otherlog = houston("other"),
+    anotherlog = houston("another"),
+    std = new houston.transports.std();
+
+houston.transports.clear().add(std);
+
+std.filter("simple", "another"); // 'other' log will not show
+
+simplelog.info("This will go to simple log");
+otherlog.info("This will go to other log");
+anotherlog.info("This will go to another log");
+
+std.filter(); // clear filters
+
+otherlog.warn("Check source code to know what is missing..");
