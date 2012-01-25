1544b03a827b0f1a43ba40713a29fbe3ce6b35aa
Diogo Resende <dresende@thinkdigital.pt>
2012-01-25 16:12:29 +0000

    Change colorlog formats to only handle what colors to use, generic log format is then used. Add houston.colors hash to be a list of possible colors (check bash colors). Fix line-number.js example to use the new colorlog format.

--- examples/line-number.js
+++ examples/line-number.js
@@ -1,6 +1,6 @@
 var houston = require("../lib/houston"),
     log = houston("simple");
 
-houston.colorLogFormat("\033[1;33m%date | %uid (line %line): %text\033[0m", "warn");
+houston.logFormat("%date | %uid (line %line): %text");
 
 log.warn("We have a problem!");
