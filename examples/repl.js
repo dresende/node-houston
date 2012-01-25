553609602aecb36ef6ae0492e6fd25fdd1181ca8
Diogo Resende <dresende@thinkdigital.pt>
2012-01-25 22:29:03 +0000

    Update repl example with an introduction comment

--- examples/repl.js
+++ examples/repl.js
@@ -1,3 +1,14 @@
+/*
+ * Run this example and then:
+ * - wait 1 second before starting to view 2 types of log messages
+ * - connect using telnet to port 9000 on your computer
+ * - type: log("module2").silence()
+ *
+ * You should stop seeing one of the log type messages. You can then:
+ * - type: log("module2").silence(false)
+ *
+ * The 2 types should now appear again.
+ */
 var houston = require("../lib/houston"),
     log1 = houston("module1"),
     log2 = houston("module2");
