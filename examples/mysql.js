e8fb688688b56a01bf04d4edfc8aad16c80723ac
Diogo Resende <dresende@thinkdigital.pt>
2012-01-26 18:18:43 +0000

    Fix mysql transport inheritance. Add data parameter to mysql transport with additional data to insert on every record. This is usefull if your table agregates from other applications or services and you need to distinguish logs with possibly same uids.

--- examples/mysql.js
+++ examples/mysql.js
@@ -8,6 +8,7 @@
 CREATE TABLE IF NOT EXISTS `myerrors` (
   `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `app` varchar(50) NOT NULL,
   `uid` varchar(50) NOT NULL,
   `text` varchar(255) NOT NULL,
   `obj` text NOT NULL,
@@ -23,7 +24,8 @@ var houston = require("../lib/houston"),
 
 houston.transports.add(new houston.transports.mysql({
 	db: { user: "myuser", password: "mypass", database: "myapp" },
-	errtable: "myerrors"
+	errtable: "myerrors",
+  data: { app: "myapp" } // <-- this is optional and that's why there is an 'app' field on the table
 }));
 
 setInterval(function () {
